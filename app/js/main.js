8fad362b209f8b5d5c57c291f7dc39c3e67e6d87
sorotiukanna <aniasorotiuk@gmail.com>
2020-05-21 19:09:17 +0300

    add js switch section

--- app/js/main.js
+++ app/js/main.js
@@ -7,84 +7,38 @@ let homeBut = document.getElementById('home');
 let aboutMeBut = document.getElementById('aboutMe');
 let worksBut = document.getElementById('myWorks');
 let contactBut = document.getElementById('contactMe');
-let fst = document.getElementById('first');
-let snd = document.getElementById('second');
-let trd = document.getElementById('third');
-let fth = document.getElementById('fourth');
+// let fst = document.getElementById('first');
+// let snd = document.getElementById('second');
+// let trd = document.getElementById('third');
+// let fth = document.getElementById('fourth');
 
-let array = [homeBut, aboutMeBut, worksBut, contactBut];
-let arrPanel = [fst, snd, trd, fth]
+let actIcon = document.getElementById('home');
+let actPanel = document.getElementById('first');
 
-function menuToggleOne(){
-    for (let i = 0; i < array.length; i++){
-        if(array[i].classList.contains('active')){
-            array[i].classList.remove('active');
-            homeBut.classList.add('active');
-        }
-    }
-    for (let j = 0; j < arrPanel.length; j++){
-        if(arrPanel[j].style.display="block"){
-            arrPanel[j].style.display="none";
-            fst.style.display="block";
-        } else{
-            fst.style.display="none";
-        }
-    }
-}
-function menuToggleTwo(){
-    for (let i = 0; i < array.length; i++){
-        if(array[i].classList.contains('active')){
-            array[i].classList.remove('active');
-            aboutMeBut.classList.add('active');
-        } 
-    }
-    for (let j = 0; j < arrPanel.length; j++){
-        if(arrPanel[j].style.display="block"){
-            arrPanel[j].style.display="none";
-            snd.style.display="block";
-        } else{
-            snd.style.display="none";
-        }
-    }
-}
-function menuToggleThree(){
-    for (let i = 0; i < array.length; i++){
-        if(array[i].classList.contains('active')){
-            array[i].classList.remove('active');
-            worksBut.classList.add('active');
-        } 
-    }
-    for (let j = 0; j < arrPanel.length; j++){
-        if(arrPanel[j].style.display="block"){
-            arrPanel[j].style.display="none";
-            trd.style.display="block";
-        } else{
-            trd.style.display="none";
-        }
-    }
-}
-function menuToggleFour(){
-    for (let i = 0; i < array.length; i++){
-        if(array[i].classList.contains('active')){
-            array[i].classList.remove('active');
-            contactBut.classList.add('active');
-        } 
-    }
-    for (let j = 0; j < arrPanel.length; j++){
-        if(arrPanel[j].style.display="block"){
-            arrPanel[j].style.display="none";
-            fth.style.display="block";
-        } else{
-            fth.style.display="none";
-        }
-    }
-}
+function menuToggle(n, m){   
 
+    actIcon.classList.toggle('active');
+    actPanel.classList.toggle('active-panel');
+
+    let x = document.getElementById(m);
+    let y = document.getElementById(n);
+
+    x.classList.toggle('active');
+    actIcon = document.getElementById(m);
+    
+    y.classList.toggle('active-panel');
+    actPanel = document.getElementById(n);
+
+}
 
 
 //-----------------------------------------------------------------------------------------------------------------------------------//
-homeBut.addEventListener('click', menuToggleOne);
-aboutMeBut.addEventListener('click', menuToggleTwo);
-worksBut.addEventListener('click', menuToggleThree);
-contactBut.addEventListener('click', menuToggleFour);
+homeBut.addEventListener('click', function(){menuToggle("first", "home")});
+aboutMeBut.addEventListener('click', function(){menuToggle("second", "aboutMe")});
+worksBut.addEventListener('click', function(){menuToggle("third", "myWorks")});
+contactBut.addEventListener('click', function(){menuToggle("fourth", "contactMe")});
+
+
+
+
 })
